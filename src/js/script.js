23f1329cc6da463ffc67af4183adf853797a7eb1
Dima <test@gmail.com>
2025-09-06 23:30:25 +0500

    scheduleBlock

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,33 @@
 // core version pagination modules:
 import Swiper from 'swiper';
-import { Pagination } from 'swiper/modules';
+import { Navigation, Pagination } from 'swiper/modules';
 
 // import Swiper and modules styles
 import 'swiper/css';
+import 'swiper/css/navigation';
 import 'swiper/css/pagination';
+
+
+try {
+	const tabs = document.querySelectorAll(".schedule__tab");
+	const contents = document.querySelectorAll(".schedule__content-item");
+
+	tabs.forEach((tab, index) => {
+		tab.addEventListener("click", () => {
+			// Удаляем активный класс у всех табов и контента
+			tabs.forEach((t) => t.classList.remove("schedule__tab_active"));
+			contents.forEach((c) => (c.style.display = "none"));
+
+			// Добавляем активный класс к нажатому табу и показываем соответствующий контент
+			tab.classList.add("schedule__tab_active");
+			contents[index].style.display = "block";
+		});
+	});
+
+	// Показываем первый контент при загрузке
+	contents.forEach((c, i) => (c.style.display = i === 0 ? "block" : "none"));
+} catch (e) {}
+
 
 try{
     const swiper = new Swiper('.swiper', {
@@ -16,6 +39,25 @@ try{
         modules: [Pagination],
 });
 } catch {}
+
+try{
+    const swiperBattles = new Swiper('.schedule__content-item', {
+        slidesPerView: 4,
+        spaceBetween: 38,
+        simulateTouch: false,
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+        navigation: {
+        nextEl: ".icon-arrow-right",
+        prevEl: ".icon-arrow-left",
+        },
+        modules: [Pagination, Navigation],
+});
+} catch {}
+
+
 
 
 import "/src/sass/style.scss";
